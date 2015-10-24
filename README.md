48cd248b1895602222839eb63e0ce36de2b323d5
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2015-10-24 15:16:23 +0100

    Initial rails project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bazar
+
 Bazar - sell your unused items
---
+++ db/migrate/20151024141135_create_items.rb
@@ -0,0 +1,9 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.integer :price
+
+      t.timestamps null: false
+    end
+  end
+end
